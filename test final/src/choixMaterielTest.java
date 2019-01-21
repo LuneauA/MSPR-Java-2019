8dae75eb1a35edb4d12aa215fee3b9592947f3f8
LuneauA <32266218+LuneauA@users.noreply.github.com>
2019-01-21 16:21:02 +0100

    Add files via upload
    Amelie

---
+++ test final/src/choixMaterielTest.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class choixMaterielTest {
+
+    private JPanel choixMateriel;
+    private JComboBox comboBox1;
+    private JButton validerButton;
+
+    public JPanel getChoixMateriel() {
+        return choixMateriel;
+    }
+
+
+    public choixMaterielTest() {
+
+    }
+
+}
+
